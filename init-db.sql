f0b620eadf297bb2acb26978d5938fcad536f092
Dilan Dilruksha <100220079+dilrukshax@users.noreply.github.com>
2025-07-27 22:17:45 +0530

    Add initial project setup with Docker and Kubernetes
    Added project configuration files including .idea settings, a README, and a build-and-deploy.sh script. Introduced docker-compose.yml for local development and multiple Kubernetes manifests for deploying services, database, and frontend. Included initial SQL for database setup and secrets/configs for environment management.

---
+++ init-db.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE user_service_db;
+CREATE DATABASE restaurant_service_db;
+CREATE DATABASE order_service_db;
+CREATE DATABASE payment_service_db;
+CREATE DATABASE delivery_service_db;
+CREATE DATABASE notification_service_db;
